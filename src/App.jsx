ec409fc43da01e4ba5e3ee31fc96a233eaacc671
GeedipalliVasu <vasu,geedipalli2005@gmail.com>
2026-05-26 23:55:52 +0530

    sec commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+//import { Github, Linkedin, Mail, ExternalLink } from 'lucide-react'
+
+export default function App() {
+
+  return (
+    <div className='Home'>
+      <nav className='navbar'>
+        <div className='container'>
+          <div className='nav-brand'>Vasu Rao</div>
+          <div className='nav-links'>
+            <a href="#projects">Projects</a>
+            <a href="#skills">Skills</a>
+            <a href="#contact">Contact</a>
+          </div>
+        </div>
+      </nav>
+
+    </div>
+  )
+}
+
